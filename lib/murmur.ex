d862b21a1597bd05d7858dbab945763cba3cf137
Gonçalo Cabrita <_@gmcabrita.com>
2014-11-08 11:18:10 +0000

    Reformat code style

--- lib/murmur.ex
+++ lib/murmur.ex
@@ -41,9 +41,9 @@ defmodule Murmur do
   @spec hash(atom, term, pos_integer) :: pos_integer
   def hash(type, data, seed \\ 0) do
    case type do
-     :x86_32  ->  hash_x86_32(data, seed)
-     :x86_128 ->  hash_x86_128(data, seed)
-     :x64_128 ->  hash_x64_128(data, seed)
+     :x86_32  -> hash_x86_32(data, seed)
+     :x86_128 -> hash_x86_128(data, seed)
+     :x64_128 -> hash_x64_128(data, seed)
    end
   end
 
@@ -52,23 +52,23 @@ defmodule Murmur do
   @spec hash_x64_128(binary, pos_integer) :: pos_integer
   defp hash_x64_128(data, seed) when is_binary(data) do
     hashes =
-      hash_64_128_aux([seed, seed], data)
-      |> Enum.zip([{31, @c1_64_128, @c2_64_128},
-                   {33, @c2_64_128, @c1_64_128}])
-      |>  Enum.map(fn ({x, {r, a, b}}) ->
-            case x do
-              {h, []} -> h ^^^ byte_size(data)
-              {h, t}  -> (h ^^^ ((swap_uint(t) * a)
-                                  |> mask_64 |> rotl64(r) |> Kernel.*(b) |> mask_64))
-                          ^^^ byte_size(data)
-            end
-          end)
+    hash_64_128_aux([seed, seed], data)
+    |> Enum.zip([{31, @c1_64_128, @c2_64_128},
+                 {33, @c2_64_128, @c1_64_128}])
+    |> Enum.map(fn ({x, {r, a, b}}) ->
+                  case x do
+                    {h, []} -> h ^^^ byte_size(data)
+                    {h, t}  -> (h ^^^ ((swap_uint(t) * a)
+                                       |> mask_64 |> rotl64(r) |> Kernel.*(b) |> mask_64))
+                               ^^^ byte_size(data)
+                  end
+                end)
 
     [h1, h2] =
-      hashes
-      |> hash_64_128_intermix
-      |> Enum.map(&fmix64/1)
-      |> hash_64_128_intermix
+    hashes
+    |> hash_64_128_intermix
+    |> Enum.map(&fmix64/1)
+    |> hash_64_128_intermix
 
     h1 <<< 64 ||| h2
   end
@@ -83,25 +83,25 @@ defmodule Murmur do
   @spec hash_x86_128(binary, pos_integer) :: pos_integer
   defp hash_x86_128(data, seed) when is_binary(data) do
     hashes =
-      hash_32_128_aux([seed, seed, seed, seed], data)
-      |> Enum.zip([{15, @c1_32_128, @c2_32_128},
-                   {16, @c2_32_128, @c3_32_128},
-                   {17, @c3_32_128, @c4_32_128},
-                   {18, @c4_32_128, @c1_32_128}])
-      |>  Enum.map(fn ({x, {r, a, b}}) ->
-            case x do
-              {h, []} -> h ^^^ byte_size(data)
-              {h, t}  -> (h ^^^ ((swap_uint(t) * a)
-                                  |> mask_32 |> rotl32(r) |> Kernel.*(b) |> mask_32))
-                          ^^^ byte_size(data)
-            end
-          end)
+    hash_32_128_aux([seed, seed, seed, seed], data)
+    |> Enum.zip([{15, @c1_32_128, @c2_32_128},
+                 {16, @c2_32_128, @c3_32_128},
+                 {17, @c3_32_128, @c4_32_128},
+                 {18, @c4_32_128, @c1_32_128}])
+    |> Enum.map(fn ({x, {r, a, b}}) ->
+                  case x do
+                    {h, []} -> h ^^^ byte_size(data)
+                    {h, t}  -> (h ^^^ ((swap_uint(t) * a)
+                                       |> mask_32 |> rotl32(r) |> Kernel.*(b) |> mask_32))
+                               ^^^ byte_size(data)
+                  end
+                end)
 
     [h1, h2, h3, h4] =
-      hashes
-      |> hash_32_128_intermix
-      |> Enum.map(&fmix32/1)
-      |> hash_32_128_intermix
+    hashes
+    |> hash_32_128_intermix
+    |> Enum.map(&fmix32/1)
+    |> hash_32_128_intermix
 
     h1 <<< 96 ||| h2 <<< 64 ||| h3 <<< 32 ||| h4
   end
@@ -116,11 +116,11 @@ defmodule Murmur do
   @spec hash_x86_32(binary, pos_integer) :: pos_integer
   defp hash_x86_32(data, seed) when is_binary(data) do
     hash =
-      case hash_32_aux(seed, data) do
-        {h, []} -> h
-        {h, t}  -> h ^^^ ((swap_uint(t) * @c1_32)
-                         |> mask_32 |> rotl32(15) |> Kernel.*(@c2_32) |> mask_32)
-      end
+    case hash_32_aux(seed, data) do
+      {h, []} -> h
+      {h, t}  -> h ^^^ ((swap_uint(t) * @c1_32)
+                        |> mask_32 |> rotl32(15) |> Kernel.*(@c2_32) |> mask_32)
+    end
 
     fmix32(hash ^^^ byte_size(data))
   end
@@ -142,15 +142,13 @@ defmodule Murmur do
 
   @spec hash_64_128_aux([pos_integer], binary) :: [{pos_integer, [binary]}]
   defp hash_64_128_aux([h1, h2],
-                      <<k1 :: size(16)-unsigned-little-integer-unit(4),
-                        k2 :: size(16)-unsigned-little-integer-unit(4),
-                        t  :: binary>>) do
-    k1 = mask_64(k1 * @c1_64_128) |> rotl64(31) |> mask_64
-          |> Kernel.*(@c2_64_128) |> mask_64
+                       <<k1 :: size(16)-unsigned-little-integer-unit(4),
+                         k2 :: size(16)-unsigned-little-integer-unit(4),
+                         t  :: binary>>) do
+    k1 = mask_64(k1 * @c1_64_128) |> rotl64(31) |> mask_64 |> Kernel.*(@c2_64_128) |> mask_64
     h1 = (((rotl64(h1 ^^^ k1, 27) + h2) * 5) + @n1_64_128) |> mask_64
 
-    k2 = mask_64(k2 * @c2_64_128) |> rotl64(33) |> mask_64
-          |> Kernel.*(@c1_64_128) |> mask_64
+    k2 = mask_64(k2 * @c2_64_128) |> rotl64(33) |> mask_64 |> Kernel.*(@c1_64_128) |> mask_64
     h2 = (((rotl64(h2 ^^^ k2, 31) + h1) * 5) + @n2_64_128) |> mask_64
 
     hash_64_128_aux([h1, h2], t)
@@ -182,45 +180,43 @@ defmodule Murmur do
 
   @spec hash_32_128_aux([pos_integer], binary) :: [{pos_integer, [binary]}]
   defp hash_32_128_aux([h1, h2, h3, h4],
-                        <<k1 :: size(8)-unsigned-little-integer-unit(4),
-                          k2 :: size(8)-unsigned-little-integer-unit(4),
-                          k3 :: size(8)-unsigned-little-integer-unit(4),
-                          k4 :: size(8)-unsigned-little-integer-unit(4),
-                          t :: binary>>) do
-    k1 = mask_32(k1 * @c1_32_128) |> rotl32(15) |> mask_32
-          |> Kernel.*(@c2_32_128) |> mask_32
+                       <<k1 :: size(8)-unsigned-little-integer-unit(4),
+                         k2 :: size(8)-unsigned-little-integer-unit(4),
+                         k3 :: size(8)-unsigned-little-integer-unit(4),
+                         k4 :: size(8)-unsigned-little-integer-unit(4),
+                         t :: binary>>) do
+    k1 = mask_32(k1 * @c1_32_128) |> rotl32(15) |> mask_32 |> Kernel.*(@c2_32_128) |> mask_32
     h1 = (((rotl32(h1 ^^^ k1, 19) + h2) * 5) + @n1_32_128) |> mask_32
 
-    k2 = mask_32(k2 * @c2_32_128) |> rotl32(16) |> mask_32
-          |> Kernel.*(@c3_32_128) |> mask_32
+    k2 = mask_32(k2 * @c2_32_128) |> rotl32(16) |> mask_32 |> Kernel.*(@c3_32_128) |> mask_32
     h2 = (((rotl32(h2 ^^^ k2, 17) + h3) * 5) + @n2_32_128) |> mask_32
 
-    k3 = mask_32(k3 * @c3_32_128) |> rotl32(17) |> mask_32
-          |> Kernel.*(@c4_32_128) |> mask_32
+    k3 = mask_32(k3 * @c3_32_128) |> rotl32(17) |> mask_32 |> Kernel.*(@c4_32_128) |> mask_32
     h3 = (((rotl32(h3 ^^^ k3, 15) + h4) * 5) + @n3_32_128) |> mask_32
 
-    k4 = mask_32(k4 * @c4_32_128) |> rotl32(18) |> mask_32
-          |> Kernel.*(@c1_32_128) |> mask_32
+    k4 = mask_32(k4 * @c4_32_128) |> rotl32(18) |> mask_32 |> Kernel.*(@c1_32_128) |> mask_32
     h4 = (((rotl32(h4 ^^^ k4, 13) + h1) * 5) + @n4_32_128) |> mask_32
 
     hash_32_128_aux([h1, h2, h3, h4], t)
   end
 
   defp hash_32_128_aux([h1, h2, h3, h4],
-                        <<t1 :: size(4)-binary, t2 :: size(4)-binary,
-                          t3 :: size(4)-binary, t  :: binary>>) do
+                       <<t1 :: size(4)-binary,
+                         t2 :: size(4)-binary,
+                         t3 :: size(4)-binary,
+                         t  :: binary>>) do
     [{h1, t1}, {h2, t2}, {h3, t3}, {h4, t}]
   end
 
   defp hash_32_128_aux([h1, h2, h3, h4],
-                        <<t1 :: size(4)-binary, t2 :: size(4)-binary,
-                          t3 :: binary>>) do
+                       <<t1 :: size(4)-binary,
+                         t2 :: size(4)-binary,
+                         t3 :: binary>>) do
     [{h1, t1}, {h2, t2}, {h3, t3}, {h4, []}]
   end
 
 
-  defp hash_32_128_aux([h1, h2, h3, h4],
-                        <<t1 :: size(4)-binary, t2 :: binary>>) do
+  defp hash_32_128_aux([h1, h2, h3, h4], <<t1 :: size(4)-binary, t2 :: binary>>) do
     [{h1, t1}, {h2, t2}, {h3, []}, {h4, []}]
   end
 
@@ -268,62 +264,62 @@ defmodule Murmur do
   # generic helper functions
 
   @spec swap_uint(binary) :: pos_integer
-  defp swap_uint(<< v1 :: size(8)-unsigned-little-integer,
-                    v2 :: size(8)-unsigned-little-integer,
-                    v3 :: size(8)-unsigned-little-integer,
-                    v4 :: size(8)-unsigned-little-integer,
-                    v5 :: size(8)-unsigned-little-integer,
-                    v6 :: size(8)-unsigned-little-integer,
-                    v7 :: size(8)-unsigned-little-integer,
-                    v8 :: size(8)-unsigned-little-integer>>) do
+  defp swap_uint(<<v1 :: size(8)-unsigned-little-integer,
+                   v2 :: size(8)-unsigned-little-integer,
+                   v3 :: size(8)-unsigned-little-integer,
+                   v4 :: size(8)-unsigned-little-integer,
+                   v5 :: size(8)-unsigned-little-integer,
+                   v6 :: size(8)-unsigned-little-integer,
+                   v7 :: size(8)-unsigned-little-integer,
+                   v8 :: size(8)-unsigned-little-integer>>) do
     (((((((v8 <<< 56) ^^^ (v7 <<< 48)) ^^^ (v6 <<< 40)) ^^^ (v5 <<< 32)) ^^^ (v4 <<< 24)) ^^^ (v3 <<< 16)) ^^^ (v2 <<< 8)) ^^^ v1
   end
 
-  defp swap_uint(<< v1 :: size(8)-unsigned-little-integer,
-                    v2 :: size(8)-unsigned-little-integer,
-                    v3 :: size(8)-unsigned-little-integer,
-                    v4 :: size(8)-unsigned-little-integer,
-                    v5 :: size(8)-unsigned-little-integer,
-                    v6 :: size(8)-unsigned-little-integer,
-                    v7 :: size(8)-unsigned-little-integer>>) do
+  defp swap_uint(<<v1 :: size(8)-unsigned-little-integer,
+                   v2 :: size(8)-unsigned-little-integer,
+                   v3 :: size(8)-unsigned-little-integer,
+                   v4 :: size(8)-unsigned-little-integer,
+                   v5 :: size(8)-unsigned-little-integer,
+                   v6 :: size(8)-unsigned-little-integer,
+                   v7 :: size(8)-unsigned-little-integer>>) do
     ((((((v7 <<< 48) ^^^ (v6 <<< 40)) ^^^ (v5 <<< 32)) ^^^ (v4 <<< 24)) ^^^ (v3 <<< 16)) ^^^ (v2 <<< 8)) ^^^ v1
   end
 
-  defp swap_uint(<< v1 :: size(8)-unsigned-little-integer,
-                    v2 :: size(8)-unsigned-little-integer,
-                    v3 :: size(8)-unsigned-little-integer,
-                    v4 :: size(8)-unsigned-little-integer,
-                    v5 :: size(8)-unsigned-little-integer,
-                    v6 :: size(8)-unsigned-little-integer>>) do
+  defp swap_uint(<<v1 :: size(8)-unsigned-little-integer,
+                   v2 :: size(8)-unsigned-little-integer,
+                   v3 :: size(8)-unsigned-little-integer,
+                   v4 :: size(8)-unsigned-little-integer,
+                   v5 :: size(8)-unsigned-little-integer,
+                   v6 :: size(8)-unsigned-little-integer>>) do
     (((((v6 <<< 40) ^^^ (v5 <<< 32)) ^^^ (v4 <<< 24)) ^^^ (v3 <<< 16)) ^^^ (v2 <<< 8)) ^^^ v1
   end
 
-  defp swap_uint(<< v1 :: size(8)-unsigned-little-integer,
-                    v2 :: size(8)-unsigned-little-integer,
-                    v3 :: size(8)-unsigned-little-integer,
-                    v4 :: size(8)-unsigned-little-integer,
-                    v5 :: size(8)-unsigned-little-integer>>) do
+  defp swap_uint(<<v1 :: size(8)-unsigned-little-integer,
+                   v2 :: size(8)-unsigned-little-integer,
+                   v3 :: size(8)-unsigned-little-integer,
+                   v4 :: size(8)-unsigned-little-integer,
+                   v5 :: size(8)-unsigned-little-integer>>) do
     ((((v5 <<< 32) ^^^ (v4 <<< 24)) ^^^ (v3 <<< 16)) ^^^ (v2 <<< 8)) ^^^ v1
   end
 
-  defp swap_uint(<< v1 :: size(8)-unsigned-little-integer,
-                    v2 :: size(8)-unsigned-little-integer,
-                    v3 :: size(8)-unsigned-little-integer,
-                    v4 :: size(8)-unsigned-little-integer >>) do
+  defp swap_uint(<<v1 :: size(8)-unsigned-little-integer,
+                   v2 :: size(8)-unsigned-little-integer,
+                   v3 :: size(8)-unsigned-little-integer,
+                   v4 :: size(8)-unsigned-little-integer>>) do
     (((v4 <<< 24) ^^^ (v3 <<< 16)) ^^^ (v2 <<< 8)) ^^^ v1
   end
-  defp swap_uint(<< v1 :: size(8)-unsigned-little-integer,
-                    v2 :: size(8)-unsigned-little-integer,
-                    v3 :: size(8)-unsigned-little-integer >>) do
+  defp swap_uint(<<v1 :: size(8)-unsigned-little-integer,
+                   v2 :: size(8)-unsigned-little-integer,
+                   v3 :: size(8)-unsigned-little-integer>>) do
     ((v3 <<< 16) ^^^ (v2 <<< 8)) ^^^ v1
   end
 
-  defp swap_uint(<< v1 :: size(8)-unsigned-little-integer,
-                    v2 :: size(8)-unsigned-little-integer >>) do
+  defp swap_uint(<<v1 :: size(8)-unsigned-little-integer,
+                   v2 :: size(8)-unsigned-little-integer>>) do
     (v2 <<< 8) ^^^ v1
   end
 
-  defp swap_uint(<< v1 :: size(8)-unsigned-little-integer >>) do
+  defp swap_uint(<<v1 :: size(8)-unsigned-little-integer>>) do
     0 ^^^ v1
   end
 
