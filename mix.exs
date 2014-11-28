1b44e1f216e43b0be55ec7154d4633675ce19b4a
Gonçalo Cabrita <_@gmcabrita.com>
2014-11-28 17:46:47 +0000

    Bump version.

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Murmur.Mixfile do
 
   def project do
     [app: :murmur,
-     version: "0.2.0",
+     version: "0.2.1",
      elixir: "~> 1.0",
      description: @description,
      package: package,
