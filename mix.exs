80bb1057aa3ce99e05b6ad5adf6a2820088f6f8b
Gonçalo Cabrita <_@gmcabrita.com>
2016-12-19 01:10:25 +0000

    Update mix.exs and dependencies.

--- mix.exs
+++ mix.exs
@@ -6,46 +6,57 @@ defmodule Murmur.Mixfile do
 
   It aims to implement the x86_32bit, x86_128bit and x64_128bit variants.
   """
+  @github "https://github.com/gmcabrita/murmur"
 
   def project do
     [
       app: :murmur,
+      name: "Murmur",
+      source_url: @github,
+      homepage_url: nil,
       version: "1.0.0",
       elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
       description: @description,
-      package: package,
-      deps: deps,
-      aliases: [
-        dialyze: "dialyze \
-                    --unmatched-returns \
-                    --error-handling \
-                    --race-conditions \
-                    --underspecs"
+      package: package(),
+      deps: deps(),
+      dialyzer: [
+        flags: ["-Wunmatched_returns", "-Werror_handling", "-Wrace_conditions", "-Wunderspecs"]
       ],
-      test_coverage: [tool: ExCoveralls]
+      test_coverage: [tool: ExCoveralls],
+      docs: docs()
     ]
   end
 
   def application do
-    [applications: []]
+    []
   end
 
-  def package do
+  defp docs do
+    [
+      main: "readme",
+      logo: nil,
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package do
     [
       files: ["lib", "mix.exs", "README.md", "LICENSE"],
       maintainers: ["Gonçalo Cabrita"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/gmcabrita/murmur"}
+      links: %{"GitHub" => @github}
     ]
   end
 
   defp deps do
     [
-      {:excoveralls, "~> 0.3", only: :docs},
-      {:earmark, "~> 0.1", only: :docs},
-      {:ex_doc, "~> 0.10", only: :docs},
-      {:inch_ex, only: :docs},
-      {:dialyze, "~> 0.2.0", only: [:dev, :test]}
+      {:excoveralls, "~> 0.5", only: :docs, runtime: false},
+      {:ex_doc, "~> 0.14", only: :docs, runtime: false},
+      {:inch_ex, ">= 0.0.0", only: :docs, runtime: false},
+      {:dialyxir, "~> 0.4", only: [:dev, :test], runtime: false},
+      {:credo, "~> 0.5", only: [:dev, :test], runtime: false}
     ]
   end
 
